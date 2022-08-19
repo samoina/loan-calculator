5681c4ceaea8a9256e0da0e6ac384742cd68abd6
Samoina <samoina.w@gmail.com>
2022-08-19 10:03:55 +0300

    Display values on the form after calculating interest values

--- app.js
+++ app.js
@@ -1,30 +1,35 @@
 //UI Variables
-let form = document.getElementById('form'),
-    loanAmount = document.getElementById('loan-amount'),
-    interestRate = document.getElementById('interest-rate'),
-    repaymentYears = document.getElementById('years'),
-    resultsDiv = document.getElementById('results'),
-    loadingDiv = document.getElementById('loading'),
-    monthlyPayments = document.getElementById('monthly-payments'),
-    totalPayment = document.getElementById('total-payment'),
-    totalInterest = document.getElementById('total-interest');
+let form = document.getElementById("form"),
+  loanAmount = document.getElementById("loan-amount"),
+  interestRate = document.getElementById("interest-rate"),
+  repaymentYears = document.getElementById("years"),
+  resultsDiv = document.getElementById("results"),
+  loadingDiv = document.getElementById("loading"),
+  monthlyPayments = document.getElementById("monthly-payments"),
+  totalPayment = document.getElementById("total-payment"),
+  totalInterest = document.getElementById("total-interest");
 
-//Hide the results div at the onset
-resultsDiv.style.display = 'none';
+//Hide the Gif & results div at the onset
+// resultsDiv.style.display = 'none';
+// loadingDiv.style.display = "none";
 
 //calculate the interest once the form is submitted
-form.addEventListener('submit', calculateInterest);
+form.addEventListener("submit", calculateInterest);
 
-function calculateInterest(event){
-  totalInterest = (loanAmount.value * interestRate.value * years.value)/100;
-  console.log(totalInterest);
+function calculateInterest(event) {
+  let interest = (loanAmount.value * interestRate.value * years.value) / 100;
+  console.log(interest);
 
-  monthlyPayments = (totalInterest/(years.value * 12)).toFixed(2);
-  console.log(monthlyPayments);
+  let totalMonthly = (interest / (years.value * 12)).toFixed(2);
+  console.log(totalMonthly);
 
-  totalPayment = parseInt(loanAmount.value) + totalInterest;
-  console.log(totalPayment);
+  let total = parseInt(loanAmount.value) + interest;
+  console.log(total);
+
+  totalInterest.value = interest;
+  totalPayment.value = total;
+  monthlyPayments.value = totalMonthly;
+
 
   event.preventDefault();
 }
-
