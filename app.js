cd09b9cd9aa894dc0b26cc17d4d0a47284e89520
Samoina <samoina.w@gmail.com>
2022-08-20 09:01:51 +0300

    Add a function that displays error if there are no values added

--- app.js
+++ app.js
@@ -7,29 +7,48 @@ let form = document.getElementById("form"),
   loadingDiv = document.getElementById("loading"),
   monthlyPayments = document.getElementById("monthly-payments"),
   totalPayment = document.getElementById("total-payment"),
-  totalInterest = document.getElementById("total-interest");
+  totalInterest = document.getElementById("total-interest"),
+  showErrorDiv = document.getElementById("show-error");
 
 //Hide the Gif & results div at the onset
 resultsDiv.style.display = "none";
 loadingDiv.style.display = "none";
 
+//showError function
+function showError() {
+  showErrorDiv.classList.add("alert", "alert-warning");
+  showErrorDiv.innerHTML = "<p>Please enter a number in the field(s) below</p>";
+
+  setTimeout(() => {
+    showErrorDiv.classList.remove("alert", "alert-warning");
+    showErrorDiv.innerHTML = "";
+  }, 2000);
+}
+
 //calculate the interest once the form is submitted
 form.addEventListener("submit", calculateInterest);
 
 function calculateInterest(event) {
+  if (
+    loanAmount.value === "" ||
+    interestRate.value === "" ||
+    repaymentYears.value === ""
+  ) {
+    showError();
+  }
+  else {
+    loadingDiv.style.display = "block";
 
-  loadingDiv.style.display = "block";
-  
   setTimeout(() => {
     let interest = (loanAmount.value * interestRate.value * years.value) / 100;
     console.log(interest);
-  
+
     let totalMonthly = (interest / (years.value * 12)).toFixed(2);
     console.log(totalMonthly);
-  
+
     let total = parseInt(loanAmount.value) + interest;
     console.log(total);
-  
+
     totalInterest.value = interest;
     totalPayment.value = total;
     monthlyPayments.value = totalMonthly;
@@ -38,7 +57,7 @@ function calculateInterest(event) {
     resultsDiv.style.display = "block";
   }, 2000);
 
- 
+  }
 
- event.preventDefault();
+  event.preventDefault();
 }
